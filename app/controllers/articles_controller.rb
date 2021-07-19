e2dba4f5351567248dcb5622fd930c9cb3995b64
BruceF1 <bruce.farthing@gmail.com>
2021-07-19 09:04:53 +0200

    added strong params

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,14 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+
+  end
+
+  def create
+    #render plain: params[:article]
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+    redirect_to @article
+  end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
-  validates :title, presence: true
-  validates :description, presence: true
+  validates :title, presence: true, length: { minimim: 6, maximum: 100}
+  validates :description, presence: true, length: { minimim: 10, maximum: 100}
 end
