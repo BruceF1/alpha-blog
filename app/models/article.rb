eb7d74bc4c2f93613315b0190b6c25db1703cf37
BruceF1 <bruce.farthing@gmail.com>
2021-08-03 09:11:23 +0200

    wip

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  belongs_to :user
   validates :title, presence: true, length: { minimim: 6, maximum: 100}
   validates :description, presence: true, length: { minimim: 10, maximum: 100}
 end
