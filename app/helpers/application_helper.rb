c938c9fc8f7be05dea0fad39a0b3c6f96748574c
BruceF1 <bruce.farthing@gmail.com>
2021-08-06 09:15:51 +0200

    add details to user profile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def robohash(user)
+    robohash_url = "https://robohash.org/#{user.email}"
+    image_tag(robohash_url, alt: user.username, class: "mx-auto d-block")
+  end
 end
