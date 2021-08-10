472d781d7296a0daacee0658a8f1b891c5768872
BruceF1 <bruce.farthing@gmail.com>
2021-08-10 09:06:55 +0200

    created user listin page - needs profile images

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,11 +13,15 @@ class UsersController < ApplicationController
     @articles = @user.articles
   end
 
+  def index
+    @users = User.all
+  end
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Yout account was successfully updated"
-      redirect_to root_path
+      redirect_to @user
     else
       render 'edit'
     end
