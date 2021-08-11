f1c8c05ee86d1233a0e12eb918fdd4c11949a8b4
BruceF1 <bruce.farthing@gmail.com>
2021-08-11 12:29:08 +0200

    add pagination to view

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,5 @@ class Article < ApplicationRecord
   belongs_to :user
   validates :title, presence: true, length: { minimim: 6, maximum: 100}
   validates :description, presence: true, length: { minimim: 10, maximum: 100}
+  paginates_per 5
 end
